87411e5992ca58a373b19608d7a5455208e3ebfe
Yuri Nunes Atzler <yuri.atzler@dbccompany.com.br>
2023-01-26 14:58:12 -0300

    add: mui e mui icon

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,36 +1,53 @@
-import style from './Login.module.css';
-
+import style from "./Login.module.css";
+import { Box } from "@mui/material";
 import { useForm } from "react-hook-form";
 
-import authAPI from '../../redux/reducers/authApi';
+import backgroundLogin from "../../assets/background-login.png";
+import backgroundHome from "../../assets/background-home.png";
+import backgroundApp from "../../assets/background-home-app.png";
 
-import { useNavigate } from 'react-router-dom';
+import authAPI from "../../redux/reducers/authApi";
 
-import { toast } from 'react-toastify';
-import { toastConfig } from '../../utils/toast';
-import { ILogin } from '../../utils/interface';
-import { sendError } from '../../utils/functions';
+import { useNavigate } from "react-router-dom";
+
+import { toast } from "react-toastify";
+import { toastConfig } from "../../utils/toast";
+import { ILogin } from "../../utils/interface";
+import { sendError } from "../../utils/functions";
 
 export const Login = () => {
   const navigate = useNavigate();
-  const [usePostAuth, { data, isLoading, isError, error  }] = authAPI.usePostAuthorizationMutation();
+  const [usePostAuth, { data, isLoading, isError, error }] =
+    authAPI.usePostAuthorizationMutation();
 
   const { register, handleSubmit } = useForm<ILogin>();
 
   const HandleLogin = (dataLogin: ILogin) => {
-    usePostAuth(dataLogin)
+    usePostAuth(dataLogin);
   };
 
-  if(isError) sendError(error);
+  if (isError) sendError(error);
 
   if (data && !isLoading && !isError) {
-    localStorage.setItem('token', data);
-    toast.success('Autenticado com sucesso', toastConfig)
+    localStorage.setItem("token", data);
+    toast.success("Autenticado com sucesso", toastConfig);
   }
 
   return (
-    <>
-      <h1>Login</h1>
+    <Box sx={{ display: "flex", maxWidth: "100vw", height: "100vh" }}>
+      <Box
+        sx={{
+          width: "50%",
+          height: "100%",
+          backgroundImage: `url(${backgroundLogin})`,
+        }}
+      ></Box>
+      <Box sx={{ width: "50%", height: "100%", bgcolor: "#f8f8ff" }}>
+        <Box
+          sx={{ display: "flex", flexDirection: "column", bgcolor: "#fff" }}
+        ></Box>
+      </Box>
+      {/* <h1>Login</h1>
       <form className={style.form} onSubmit={handleSubmit(HandleLogin)}>
         <div className={style.div}>
           <input type="text" {...register("email")} required placeholder='Email' />
@@ -38,7 +55,7 @@ export const Login = () => {
         </div>
         <button type="submit">Fazer login</button>
       </form>
-      <button onClick={() => navigate('/inicial')}>Home</button>
-    </>
-  )
-}
+      <button onClick={() => navigate('/inicial')}>Home</button> */}
+    </Box>
+  );
+};
